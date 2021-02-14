66ea63743cc3d0638f117c0d58cb5f8965d40d9b
PedroEdu6786 <pedrcg835@gmail.com>
2021-02-14 15:07:14 -0600

    feat: move to generated project folder, initialized git repo

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import sys
 from src.validators.ArgsValidation import ArgsValidation
 from src.model.FolderGenerator import FolderGenerator
+from src.model.GitHandler import GitHandler
 
 
 if __name__ == "__main__":
     num_args = len(sys.argv)
     
-    print(num_args)
     if not ArgsValidation.validate_num_args(num_args):
         print("Number of arguments not valid")
         sys.exit()
@@ -15,4 +15,6 @@ if __name__ == "__main__":
     
     folder_name = FolderGenerator()
     folder_name.generate_folder(file_argument)
-    
+    folder_name.navigate_to_folder(file_argument)
+    git = GitHandler()
+    git.create_new_repo()
--- src/model/FolderGenerator.py
+++ src/model/FolderGenerator.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 class FolderGenerator:
         
@@ -12,5 +13,9 @@ class FolderGenerator:
             os.mkdir(folder_path)
         except OSError:
             print(f'Error to create folder {folder_name}')
+            sys.exit()
             
         print(f'Successfully created folder {folder_name}')
+        
+    def navigate_to_folder(self, folder_name):
+        os.chdir(f'{self.path}/{folder_name}')
